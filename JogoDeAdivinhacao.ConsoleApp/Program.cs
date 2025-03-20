35b780e2b33bd6d1a71d216aa7ae7a08bdc1ffb8
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-03-20 20:35:43 -0300

    Implementação básica do jogo da adivinhação.

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" ---------------------------------------------");
+            Console.WriteLine(" Jogo de adivinhação");
+            Console.WriteLine(" ---------------------------------------------\n");
+
+            Random geradorDeNumeros = new Random();
+
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+            Console.Write("Digite um número entre 1 á 20 para a adivinhação: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            
+            if (numeroDigitado == numeroSecreto)
+            {
+                Console.WriteLine(" ---------------------------------------------");
+                Console.WriteLine(" Parabéns, você acertou!");
+                Console.WriteLine(" ---------------------------------------------\n");
+            }
+            else if (numeroDigitado > numeroSecreto)
+            {
+                Console.WriteLine(" ---------------------------------------------");
+                Console.WriteLine(" O número digitado foi maior que o número secreto!");
+                Console.WriteLine(" ---------------------------------------------\n");
+            }
+            else if (numeroDigitado < numeroSecreto)
+            {
+                Console.WriteLine(" ---------------------------------------------");
+                Console.WriteLine(" O número digitado foi menor que o número secreto!");
+                Console.WriteLine(" ---------------------------------------------\n");
+            }
+
+
+                Console.ReadLine();
         }
     }
 }
